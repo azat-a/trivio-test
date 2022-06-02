e31d94cb088eb411a73bd10d0b0333afa3c687bb
Азат Аюпов <aiupov@trueconf.com>
2022-06-03 02:07:38 +0300

    ops: replace svg loading rule, add inline loading

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
 const { defineConfig } = require('@vue/cli-service');
 
+/* eslint-disable */
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.clear();
+
+    svgRule
+      .test(/\.svg$/i)
+      .oneOf('inline')
+        .resourceQuery(/inline/)
+        .type('asset/source')
+        .end()
+      .oneOf('file')
+        .type('asset')
+        .set('generator', {
+          filename: 'img/[name].[hash:8][ext]'
+        })
+  },
 });
